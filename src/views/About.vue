37f4172d890d0d09952886326542d429cd12f93b
Alexey <alexeyerpd@gmail.com>
2018-11-27 17:53:02 +0300

    upd

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+
+        <img src="/img/background.png" alt="dd" />
+        <img src="/static/img/background.png" alt="dd2" />
+        <img src="" alt="dd3" />
+        <div>Привет</div>
+        <div>Пока</div>
+    </div>
+</template>
+
+<script>
+// import "././";
+
+export default {};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    assetsDir: undefined,
+    baseUrl: undefined,
+    outputDir: undefined,
+    runtimeCompiler: undefined,
+    productionSourceMap: false,
+    parallel: undefined,
+    css: undefined
+};
